0c8415a6e36df417819418841562b4e36dd0cf78
SanskarDakhore <sanskardakhore1@gmail.com>
2025-05-19 16:24:46 +0530

    all

--- spotify_backend/utils/helpers.js
+++ spotify_backend/utils/helpers.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 exports={}
 exports.getToken= async()=>{
 // assume this code is complete. 
-const token = Jwt.sign({identifier : userModel._id});
+const token = Jwt.sign({identifier : userModel._id},process.env.JWT_PASSWORD);
 return token ; 
 };
 module.exports=exports
